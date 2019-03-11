361cac811fe31a6fdb8fbca7c8d8950945122f89
dorothy.kwon@blueindex.com <dorothy.kwon@blueindex.com>
2019-03-11 19:25:29 +0900

    플러그인추가 - year데이터 피커를 위한 jquery-ui플러그인 추가

--- js/ui_mobile.js
+++ js/ui_mobile.js
@@ -9,4 +9,19 @@ $(function(){
           $("#menu,.page_blur,html").removeClass("open");
         }
       };
+
+
+      // year picker
+      $('#datepicker').datepicker({
+        changeYear: true,
+        showButtonPanel: true,
+        dateFormat: 'yy',
+        onClose: function(dateText, inst) { 
+          var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+          $(this).datepicker('setDate', new Date(year, 1));
+        }
+      });
+      $(".date-picker-year").focus(function () {
+        $(".ui-datepicker-month").hide();
+      });
 });
