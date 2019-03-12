ee79a7723f57a133eb1e6e78c267259621d725e5
dorothy.kwon@blueindex.com <dorothy.kwon@blueindex.com>
2019-03-12 10:33:47 +0900

    작업페이지 리스트 추가 / 필터 수정중...

--- js/ui_mobile.js
+++ js/ui_mobile.js
@@ -1,26 +1,39 @@
 $(function(){
     $(".list_filter a").click(function() {
-        $("#menu,.page_blur,html").addClass("open");
+        $(".m_slider,.page_blur,html").addClass("open");
         window.location.hash = "#open";
       });
       
       window.onhashchange = function() {
         if (location.hash != "#open") {
-          $("#menu,.page_blur,html").removeClass("open");
+          $(".m_slider,.page_blur,html").removeClass("open");
         }
       };
 
 
       // year picker
-      $('#datepicker').datepicker({
+      $('#picker_year_start').datepicker({
         changeYear: true,
         showButtonPanel: true,
         dateFormat: 'yy',
+        yearRange: "-500:+0",
         onClose: function(dateText, inst) { 
           var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
           $(this).datepicker('setDate', new Date(year, 1));
         }
       });
+
+      $('#picker_year_end').datepicker({
+        changeYear: true,
+        showButtonPanel: true,
+        dateFormat: 'yy',
+        yearRange: "-500:+0",
+        onClose: function(dateText, inst) { 
+          var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+          $(this).datepicker('setDate', new Date(year, 1));
+        }
+      });
+
       $(".date-picker-year").focus(function () {
         $(".ui-datepicker-month").hide();
       });
