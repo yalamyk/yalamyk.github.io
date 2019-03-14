ff6f065aaac88ae0f906d194bfa58c5262295d9e
dorothy.kwon@blueindex.com <dorothy.kwon@blueindex.com>
2019-03-14 11:04:37 +0900

    03 filter 영역 레이아웃( sort 추가 및 filter 편집 )

--- js/ui_mobile.js
+++ js/ui_mobile.js
@@ -1,9 +1,9 @@
 $(function(){
-    $(".list_filter a").click(function() {
-        $(".m_slider, .page_blur, html").addClass("open");
-        window.location.hash = "#open";
-        console.log(' window.location.hash : '+window.location.hash);
-      }); 
+    // $(".list_filter a").click(function() {
+    //     $(".m_slider, .page_blur, html").addClass("open");
+    //     window.location.hash = "#open";
+    //     console.log(' window.location.hash : '+window.location.hash);
+    //   }); 
       // window.onhashchange = function() {
       //   console.log('onhashchange : '+window.location.hash);
       //   if (window.location.hash != "#open") {
@@ -12,27 +12,27 @@ $(function(){
       // };
 
       // year picker
-      $('#picker_year_start').datepicker({
-        changeYear: true,
-        showButtonPanel: true,
-        dateFormat: 'yy',
-        yearRange: "-500:+0",
-        onClose: function(dateText, inst) { 
-          var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
-          $(this).datepicker('setDate', new Date(year, 1));
-        }
-      });
+      // $('#picker_year_start').datepicker({
+      //   changeYear: true,
+      //   showButtonPanel: true,
+      //   dateFormat: 'yy',
+      //   yearRange: "-500:+0",
+      //   onClose: function(dateText, inst) { 
+      //     var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+      //     $(this).datepicker('setDate', new Date(year, 1));
+      //   }
+      // });
 
-      $('#picker_year_end').datepicker({
-        changeYear: true,
-        showButtonPanel: true,
-        dateFormat: 'yy',
-        yearRange: "-500:+0",
-        onClose: function(dateText, inst) { 
-          var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
-          $(this).datepicker('setDate', new Date(year, 1));
-        }
-      });
+      // $('#picker_year_end').datepicker({
+      //   changeYear: true,
+      //   showButtonPanel: true,
+      //   dateFormat: 'yy',
+      //   yearRange: "-500:+0",
+      //   onClose: function(dateText, inst) { 
+      //     var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+      //     $(this).datepicker('setDate', new Date(year, 1));
+      //   }
+      // });
 
       $(".date-picker-year").focus(function () {
         $(".ui-datepicker-month").hide();
