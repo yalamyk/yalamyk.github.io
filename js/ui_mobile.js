e6b4a4093f97ce455ebcf6c5ea3938323c3f64b0
dorothy.kwon@blueindex.com <dorothy.kwon@blueindex.com>
2019-03-19 19:02:17 +0900

    (공통수정사항 메인 화면 / 로그인 화면) title 수정 / input 수정중(js 안나옴)

--- js/ui_mobile.js
+++ js/ui_mobile.js
@@ -10,56 +10,17 @@ $(function(){
       //     $(".m_slider,.page_blur,html").removeClass("open");
       //   }
       // };
-
-      // year picker
-      // $('#picker_year_start').datepicker({
-      //   changeYear: true,
-      //   showButtonPanel: true,
-      //   dateFormat: 'yy',
-      //   yearRange: "-500:+0",
-      //   onClose: function(dateText, inst) { 
-      //     var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
-      //     $(this).datepicker('setDate', new Date(year, 1));
-      //   }
-      // });
-
-      // $('#picker_year_end').datepicker({
-      //   changeYear: true,
-      //   showButtonPanel: true,
-      //   dateFormat: 'yy',
-      //   yearRange: "-500:+0",
-      //   onClose: function(dateText, inst) { 
-      //     var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
-      //     $(this).datepicker('setDate', new Date(year, 1));
-      //   }
-      // });
-
-
-// jQuery(function(){
-// var footerAbsoluteBottom = function(){
-// var windowHeight = $(window).outerHeight();
-// var footerHeight = $('#footer').outerHeight();
-// $('#wrapper').css(
-// {
-// 'min-height' : windowHeight,
-// 'padding-bottom' : footerHeight
-// }
-// );
-// }
-// $(window).resize(function(){
-// footerAbsoluteBottom();
-// });
-// $(document).ready(function(){
-// footerAbsoluteBottom();
-// });
-// });
-// <style>
-// #wrapper{position:relative;}
-// #footer{position:absolute;left:0;bottom:0;width:100%;}
-// </style>
-
-
-      $(".date-picker-year").focus(function () {
-        $(".ui-datepicker-month").hide();
-      });
+      console.log('ui_mobile is load');
+      
+});
+$(document).ready(function(){
+  function isValueTrue(num) {
+    console.log(num);
+    var num = document.getElementById(num).value;
+    if(num){
+      return true;
+    }else{
+      return false;
+    }
+  }      
 });
