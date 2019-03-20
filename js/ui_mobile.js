1ad0df815fc1414fd255d718360f7da946163aae
dorothy.kwon@blueindex.com <dorothy.kwon@blueindex.com>
2019-03-20 15:41:29 +0900

    datarangepicker 추가중 / 아트워크디테일 수정중

--- js/ui_mobile.js
+++ js/ui_mobile.js
@@ -1,15 +1,15 @@
 $(function(){
-    // $(".list_filter a").click(function() {
-    //     $(".m_slider, .page_blur, html").addClass("open");
-    //     window.location.hash = "#open";
-    //     console.log(' window.location.hash : '+window.location.hash);
-    //   }); 
-      // window.onhashchange = function() {
-      //   console.log('onhashchange : '+window.location.hash);
-      //   if (window.location.hash != "#open") {
-      //     $(".m_slider,.page_blur,html").removeClass("open");
-      //   }
-      // };
+    $(".list_filter a").click(function() {
+        $(".m_slider, .page_blur, html").addClass("open");
+        window.location.hash = "#open";
+        console.log(' window.location.hash : '+window.location.hash);
+      }); 
+      window.onhashchange = function() {
+        console.log('onhashchange : '+window.location.hash);
+        // if (window.location.hash !== "#open") {
+        //   $(".m_slider,.page_blur,html").removeClass("open");
+        // }
+      };
       console.log('ui_mobile is load');
       
       function isValueTrue(num) {
